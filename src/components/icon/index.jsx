ad33740661da0535808d8408afd369253d01c76f
ThanhPhongsof <ttphongletter@gmail.com>
2022-10-01 09:56:12 +0700

    fix/main : update section testimonial in desktop

--- src/components/icon/index.jsx
+++ src/components/icon/index.jsx
@@ -4,3 +4,4 @@ export const { default: IconRent } = require("./IconRent");
 export const { default: IconPencil } = require("./IconPencil");
 export const { default: IconCreditCard } = require("./IconCreditCard");
 export const { default: IconSpeeddomito } = require("./IconSpeeddomito");
+export const { default: IconStar } = require("./IconStar");
--- src/modules/main/Testimonial.jsx
+++ src/modules/main/Testimonial.jsx
@@ -1,8 +1,59 @@
+import { IconStar } from "components/icon";
+import { LayoutContainer } from "components/layout";
+import { v4 } from "uuid";
+
+const testimonialList = [
+  {
+    img: "/testimonial-img-01.png 2x",
+    name: "Taras Migulko",
+    job: "Businessman",
+    quote:
+      "Coldwell Banker affiliated agents know the value of home, the place we long for at the",
+    stars: 5,
+  },
+  {
+    img: "/testimonial-img-02.png 2x",
+    name: "Guy Mckinney",
+    job: "Real Estate Agent",
+    quote:
+      "Coldwell Banker affiliated agents know the value of home, the place we long for at the",
+    stars: 3,
+  },
+  {
+    img: "/testimonial-img-03.png 2x",
+    name: "Dwight Steward",
+    job: "Real Estate Agent",
+    quote:
+      "Coldwell Banker affiliated agents know the value of home, the place we long for at the",
+    stars: 4,
+  },
+];
 
 const Testimonial = () => {
   return (
     <section className="testimonial">
-      Testimonial
+      <LayoutContainer>
+        <h3 className="testimonial-heading heading heading--medium">
+          Here what out customers have to say
+        </h3>
+        <div className="testimonial-list">
+          {testimonialList?.map((item) => (
+            <div key={v4()} className="testimonial-item">
+              <img srcSet={item.img} alt={item.title} />
+              <h4 className="testimonial-name heading heading--small">
+                {item.title}
+              </h4>
+              <p className="testimonial-job text">{item.job}</p>
+              <p className="testimonial-quote">{item.quote}</p>
+              <div className="testimonial-stars">
+                {new Array(item.stars).fill(0).map(() => (
+                  <IconStar key={v4()} />
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </LayoutContainer>
     </section>
   );
 };
